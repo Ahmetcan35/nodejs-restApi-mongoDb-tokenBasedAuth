deb54494831a7ee6385bd673d5b3ceb1bac13e25
Ahmetcan35 <ahmetcancetinz@gmail.com>
2024-10-15 02:58:55 +0300

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV == "production") {
 }
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || "3000";
 
 app.listen(port,  () => {
     console.log(`listening on port ${port}`);
