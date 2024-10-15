02ae7ab9883ebc674ef40184906587149043aedd
Ahmetcan35 <ahmetcancetinz@gmail.com>
2024-10-15 12:16:43 +0300

    changes

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
 require("./startup/logger");
+require("./startup/routes")(app);
 require("./startup/db")();
 
-require("./startup/routes")(app);
 if (process.env.NODE_ENV == "production") {
-    require("./startup/production");
+    require("./startup/production")(app);
     
 }
 
 
+const port = process.env.PORT || "3000";    
 
 app.listen(process.env.PORT || 3000,  () => {
     console.log(`listening on port ${port}`);
